fd3ed2d570377970fc30c19c862c4e51028ba2a2
AlejandroManzaneraRamon <alejandromanzanera@iesebre.com>
2023-10-30 19:40:30 +0100

    Sync

---
+++ app/index.php
@@ -0,0 +1,7 @@
+<?php
+//echo "Hello World"
+
+require "MP09/app/index.php";
+
+require "MP09/resources/views/index.blade.php";
+
---
+++ resources/views/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mi Página Web</title>
+</head>
+<body>
+<h1>Bienvenido a Mi Página Web</h1>
+<p>Esta es una página web de ejemplo.</p>
+
+<h2>Secciones</h2>
+<ul>
+    <li><a href="#seccion1">Sección 1</a></li>
+    <li><a href="#seccion2">Sección 2</a></li>
+</ul>
+
+<h3 id="seccion1">Sección 1</h3>
+<p>Contenido de la Sección 1.</p>
+
+<h3 id="seccion2">Sección 2</h3>
+<p>Contenido de la Sección 2.</p>
+
+<footer>
+    <p>&copy; 2023 Mi Página Web</p>
+</footer>
+</body>
+</html>
